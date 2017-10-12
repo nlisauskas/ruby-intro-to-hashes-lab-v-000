b3f5eba85c34fa267052b12a2bf5dd48ace69d7d
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-12 23:25:17 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,13 +39,13 @@ def monopoly_with_third_tier
 					
 				},
 				pennsylvania_railroad: {
-					
+
 				},
 				b_and_o_railroad: {
-					
+
 				},
 				shortline: {
-					
+
 				}
 			},
 			rent_in_dollars: {
