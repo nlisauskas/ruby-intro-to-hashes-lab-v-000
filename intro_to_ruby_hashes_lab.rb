3a8e965abe674defb36f05a6a458e2365978e72f
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-12 23:20:28 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,13 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 		railroads: {
-			pieces: 4
+			pieces: 4,
+			names: {
+				
+			},
+			rent_in_dollars: {
+				one_piece_owned: 25
+			}
 		}
 	}
 
